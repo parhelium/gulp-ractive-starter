aaa4021c234a7e2918922ac8212767e437f477e2
parhelium <me@parhelium.pl>
2014-08-17 03:20:57 +0200

    adding 'ract' component

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,11 +1,23 @@
-
 var Ractive = require('ractive');
 
-var ractive = new Ractive({
+//var ractive = new Ractive({
+//    el: '#output',
+//    template: require('../templates/Clock.ract').template,
+////    template: require('../templates/template.html'),
+//    data: {
+//        name: 'World'
+//    }
+//});
+
+
+var Clock = Ractive.extend(require('../templates/Clock.ract'));
+new Clock({
     el: '#output',
-    template: require('../templates/template.html'),
-    data: {
-        name: 'World'
-    }
+    append:true
 });
 
+new Ractive({
+    template:"OKO",
+    el:"#output",
+    append:true
+})
