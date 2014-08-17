6bfd363b2fd094c2a72b4c41aacda9d4e6bc3978
parhelium <me@parhelium.pl>
2014-08-17 02:31:31 +0200

    added rule to copy css files

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,5 +3,6 @@ var gulp = require('gulp');
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
 	gulp.watch('src/images/**', ['images']);
 	gulp.watch('src/htdocs/**', ['copy']);
+	gulp.watch('src/css/**', ['copy']);
 	// Note: The browserify task handles js recompiling with watchify
 });
