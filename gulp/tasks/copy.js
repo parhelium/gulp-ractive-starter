9c4c724d76101e36425d9e6374402d51f657e470
parhelium <me@parhelium.pl>
2014-08-18 10:40:01 +0200

    refactoring : restructured files

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('copy', function() {
-	return gulp.src(['src/htdocs/**','src/css/**'])
+	return gulp.src(['src/html/**','src/css/**'])
 		.pipe(gulp.dest('build'));
 });
